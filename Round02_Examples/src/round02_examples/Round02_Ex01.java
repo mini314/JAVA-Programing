ea9b862b2fe0ca8dfb412d9b6a5b820677989e6d
mini <mini@172.16.62.138>
2014-10-31 16:26:13 +0900

    Round02 examples 30 Oct 2014

---
+++ Round02_Examples/src/round02_examples/Round02_Ex01.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package round02_examples;
+
+/**
+ *
+ * @author mini
+ */
+import java.lang.*;
+public class Round02_Ex01 {
+    public static void main(String[] ar){
+        System.out.println("Hello Java!");
+    }
+}
---
+++ Round02_Examples/src/round02_examples/Round02_HW.java
@@ -0,0 +1,16 @@
+package round02_examples;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author mini
+ */
+public class Round02_HW {
+    public static void main(String[] ar){
+        System.out.println("안녕하세요! 여러분!\n자바 공부를 열심히 합시다.\n모두들 화이링요^^");
+    }
+}
